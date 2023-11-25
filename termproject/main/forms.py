9c1e18a2e15e027f50f1aff284315923b87a2d1f
Mingyeong Jung <77571090+kkong1007@users.noreply.github.com>
2023-11-25 20:03:50 +0900

    Update forms.py

--- termproject/main/forms.py
+++ termproject/main/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 
+op_code = [
+    ('출고', False),
+    ('입고', True),
+]
 
-class NewItemFrom(forms.Form):
+
+class NewItemForm(forms.Form):
     name = forms.CharField(max_length=100)
     weight = forms.FloatField()
     category_id = forms.IntegerField()
     stocked_date = forms.DateField()
+
+
+class NewStockForm(forms.Form):
+    op_code = forms.BooleanField()
+    weight = forms.FloatField()
+    stock_date = forms.DateField()
