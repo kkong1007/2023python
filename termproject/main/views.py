d542c14eb938d646243fc43b7e9e9186a56b41e5
Mingyeong Jung <77571090+kkong1007@users.noreply.github.com>
2023-11-25 20:04:44 +0900

    Update views.py

--- termproject/main/views.py
+++ termproject/main/views.py
@@ -1,7 +1,10 @@
+from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Item, Category, ItemStoredHistory
-from .forms import NewItemFrom
+from .forms import NewItemForm, NewStockForm
 
 
 def main(request):
@@ -11,8 +14,8 @@ def main(request):
     })
 
 
-def detail(request, id):
-    histories = ItemStoredHistory.objects.filter(item_id=id).order_by("-id")
+def detail(request, pk):
+    histories = ItemStoredHistory.objects.filter(item_id=pk).order_by("-id")
 
     date_list = [
         {
@@ -34,19 +37,53 @@ def detail(request, id):
 def create(request):
     if request.method == 'GET':
         category_list = Category.objects.all()
-        new_form = NewItemFrom()
+        new_form = NewItemForm()
         return render(request, 'create.html', context={
             'form': new_form,
             'category_list': category_list
         })
     elif request.method == 'POST':
-        form = NewItemFrom(request.POST)
+        form = NewItemForm(request.POST)
 
         if form.is_valid():
             new_item = Item.objects.create(name=form.cleaned_data.get('name'), weight=form.cleaned_data.get('weight'),
-                                         category_id=form.cleaned_data.get('category_id'),
-                                         stocked_date=form.cleaned_data.get('stocked_date'))
+                                           category_id=form.cleaned_data.get('category_id'),
+                                           stocked_date=form.cleaned_data.get('stocked_date'))
 
             ItemStoredHistory.objects.create(item=new_item, weight=form.cleaned_data.get('weight'))
 
             return redirect(to="item:list")
+
+@transaction.atomic()
+@csrf_exempt
+def stock(request, pk):
+    if request.method == 'POST':
+        form = NewStockForm(request.POST)
+        if form.is_valid():
+            find_item = get_object_or_404(Item, id=pk)
+
+            op_code = form.cleaned_data.get('op_code')
+            weight = form.cleaned_data.get('weight')
+
+            if (op_code):
+                find_item.weight += weight
+            else:
+                find_item.weight -= weight
+
+            find_item.save()
+            ItemStoredHistory.objects.create(item=find_item,
+                                             weight=weight,
+                                             op_code=op_code,
+                                             created_at=form.cleaned_data.get('stock_date'))
+            return JsonResponse(data={
+                "message": "성공"
+            }, status=200)
+        else:
+            return JsonResponse(data={
+                "message": f"실패: {form.errors}"
+            }, status=400)
+    else:
+        result = {
+            'message': 'POST만 가능'
+        }
+        return JsonResponse(result, status=400)
