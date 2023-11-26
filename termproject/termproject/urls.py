7adfa24def8cf4588e9bad072a14be082b16ceaa
Mingyeong Jung <77571090+kkong1007@users.noreply.github.com>
2023-11-26 17:53:59 +0900

    Update urls.py

--- termproject/termproject/urls.py
+++ termproject/termproject/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
+from termproject import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('/items/', request)),
-    path('items/', include('main.urls')),
+    path('items/', include('main.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ]
