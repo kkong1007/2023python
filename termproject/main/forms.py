99ccf61dceda94345b5720d237ecd19e1cd9c1c1
Mingyeong Jung <77571090+kkong1007@users.noreply.github.com>
2023-11-26 17:52:28 +0900

    Update forms.py

--- termproject/main/forms.py
+++ termproject/main/forms.py
@@ -9,11 +9,12 @@ op_code = [
 class NewItemForm(forms.Form):
     name = forms.CharField(max_length=100)
     weight = forms.FloatField()
+    minimum_weight = forms.FloatField()
     category_id = forms.IntegerField()
-    stocked_date = forms.DateField()
+    stocked_date = forms.DateField(input_formats=['%Y-%m-%dT%H:%M'])
 
 
 class NewStockForm(forms.Form):
     op_code = forms.BooleanField()
     weight = forms.FloatField()
-    stock_date = forms.DateField()
+    stock_date = forms.DateField(input_formats=['%Y-%m-%dT%H:%M'])
