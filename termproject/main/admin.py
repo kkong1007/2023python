1b98b25b2e55cd907afe0ddc2ae1a2295c8c141d
Mingyeong Jung <77571090+kkong1007@users.noreply.github.com>
2023-11-26 17:52:14 +0900

    Update admin.py

--- termproject/main/admin.py
+++ termproject/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Item, ItemStoredHistory
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ItemStoredHistoryInline(admin.TabularInline):
+    model = ItemStoredHistory
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemStoredHistoryInline,
+    ]
